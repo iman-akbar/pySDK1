03f7e79d401970e786b73d2517bf531aff047ea1
iman.akbar <iman.akbar@mncgroup.comv>
2021-11-08 13:41:28 +0700

    proper

--- source.py
+++ source.py
@@ -1,5 +1,5 @@
 class trySDK():
-    favs = [] #class
+    favs = [] 
 
     def __init__(self, title, pages):
         self.title = title
@@ -9,18 +9,17 @@ class trySDK():
         if self.pages < 100:
             return true
 
-    #What happens when you pass object to print?
+   
     def __str__(self):
         return f"{self.title}, {self.pages} pages long"
 
-    #What happens when you use ==?
+    
     def __eq__(self, other):
         if(self.title == other.title and self.pages == other.pages):
             return True
     
-    #It's approriate to give something for __hash__ when you override __eq__
-    # #This is the recommended way if mutable (like it is here):
+    
     __hash__ = None
 
-    def __repr__(self): #added to make list of items invoke str
+    def __repr__(self): 
         return self.__str__()
